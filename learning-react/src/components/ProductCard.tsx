ce061c7ec1327cc4bc0ccef37c3823295a2e93c3
krishnareddy-tech007 <krishna@northpartners.ai>
2026-07-14 21:55:16 +0530

    Array Rendering by passing data to child component

---
+++ learning-react/src/components/ProductCard.tsx
@@ -0,0 +1,39 @@
+import type { Product } from "../App";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+function ProductCard({ product }: ProductCardProps) {
+  return (
+    <div className="card shadow h-100">
+      <div className="card-body p-4">
+
+        <h3 className="card-title text-center mb-4">
+          {product.title}
+        </h3>
+
+        <p className="card-text">
+          Author: <strong>{product.author}</strong>
+        </p>
+
+        <p className="card-text">
+          Publisher: <strong>{product.publisher}</strong>
+        </p>
+
+        <p className="card-text">
+          Price: <strong>{product.price}</strong>
+
+          {product.price < 400 && (
+            <span className="text-success ms-2">
+              - Bestseller!
+            </span>
+          )}
+        </p>
+
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
